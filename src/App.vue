325c26f2f3dc089798d2b82951a09958aed05259
kaankucuk <kaankck@gmail.com>
2019-03-10 00:27:48 +0300

    info mail added

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
     <!--<div id="nav">-->
-      <!--<router-link to="/">Home</router-link> |-->
-      <!--<router-link to="/about">About</router-link>-->
+    <!--<router-link to="/">Home</router-link> |-->
+    <!--<router-link to="/about">About</router-link>-->
     <!--</div>-->
     <div class="overlay"></div>
-    <router-view/>
+    <router-view />
+    <p class="contact">info@yedincibasamak.com</p>
   </div>
 </template>
 
 <style lang="scss">
-  html, body, #app {
-    height: 100%;
-    margin: 0;
-  }
+html,
+body,
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  height: 100%;
+  margin: 0;
+  background-color: #65d381;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -33,6 +37,13 @@
   left: 0;
   width: 100%;
   height: 100%;
+}
+.contact {
+  color: hsl(169, 37%, 11%);
+  font-size: 35px;
+  font-weight: 900;
+  letter-spacing: -1.4px;
+  margin: 0;
 }
 #nav {
   padding: 30px;
